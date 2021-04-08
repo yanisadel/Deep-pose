c8e178cf65d33f53b89c66426922c49ccbe1c612
mehdidahmani56 <mehdi.dahmani@student-cs.fr>
2021-04-08 11:52:00 +0200

    ajouter un origin

--- predictions.py
+++ predictions.py
@@ -26,6 +26,60 @@ train_knn(knn,x_train,y_train)
 
 predictions = predictions_knn(knn, x_test)
 
+def show_results (y_test, predictions) :
+    """
+    Retourne un Pandas Dataframe de la forme :
+
+                  geste  prediction
+            52       3           3
+            182      8           8
+            5        1           1
+            18       1           1
+
+    Arguments
+    ---------
+    y_test 
+    predictions 
+
+    Returns
+    -------
+    df: DataFrame
+        
+    
+    """
+    index = y_test.index.to_numpy()
+    y = y_test.array.to_numpy()
+    df = pd.DataFrame({ 'geste': pd.Series(y, index = index), 'prediction' : pd.Series(predictions, index = index)})
+    return df
+
+def echecs (y_test, predictions): 
+    """ 
+    Calcule le pourcentage d'echecs dans le test
+
+    Arguments
+    ---------
+       y_test 
+       predictions 
+
+    Returns
+    -------
+    echec : float
+           pourcentage d'echecs
+    """
+    df = show_results(y_test,predictions)
+    ges = df['geste'].array.to_numpy()
+    pre = df['prediction'].array.to_numpy()
+    n = len(df)
+    p = 0
+    for i in range(n):
+        if ges[i] != pre[i]:
+            p+=1
+    echec = p*100/n
+    return echec
+
+# Execution 
+print(show_results(y_test,predictions))
+print("Le pourcentage d'échecs est : "+ str(int(echecs(y_test,predictions)))+'%')            
 
 
 
